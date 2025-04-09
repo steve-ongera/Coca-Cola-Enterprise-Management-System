849a14c3acc8fa10beca31c30f036d8349a1a943
steve-ongera <gadafisteve001@gmail.com>
2025-04-09 17:13:02 +0300

    added changes

--- COCA_COLA/urls.py
+++ COCA_COLA/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include("mywebsite.urls")),
+    path('', include("mywebsite.urls")),
 ]
 
 
