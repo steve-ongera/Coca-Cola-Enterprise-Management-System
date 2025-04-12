f8f1c2771192522c54760ef9e8366b39925a3924
steve-ongera <gadafisteve001@gmail.com>
2025-04-12 21:13:27 +0300

    made modifications

--- mywebsite/models.py
+++ mywebsite/models.py
@@ -716,6 +716,7 @@ class MaintenanceRecord(models.Model):
     cost = models.DecimalField(max_digits=10,decimal_places=2,null=True,blank=True,validators=[MinValueValidator(0)])
     created_by = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    
 
     class Meta:
         ordering = ['-date']
